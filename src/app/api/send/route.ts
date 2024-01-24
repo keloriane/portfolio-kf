3977981ef15b4ad4ef0e960dfe88cd08051fecf4
Kevin Flabat <kevin.flbt@gmail.com>
2024-01-24 17:02:32 +0100

    .env + responsive

--- src/app/api/send/route.ts
+++ src/app/api/send/route.ts
@@ -2,7 +2,7 @@ import { NextResponse } from "next/server";
 import { Resend } from "resend";
 
 export async function GET() {
-    const resend = new Resend(process.env.SEND_API_KEY);
+    const resend = new Resend(process.env.RESEND_API_KEY);
     try {
         const { data } = await resend.emails.send({
             from: 'onboarding@resend.dev',
