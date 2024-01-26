2bd8338c456eda73588b4a5c0122c19ab69e707a
Kevin Flabat <kevin.flbt@gmail.com>
2024-01-26 16:50:32 +0100

    form submisison

--- src/app/api/send/route.ts
+++ src/app/api/send/route.ts
@@ -20,33 +20,9 @@ export async function POST(request:Request) {
             html: `<p>Message from ${firstname} ${lastname} : ${message} <br/> form mail: ${email} </p>`
         });
 
-        if(data.status === 'success') {
-            return NextResponse.json({message: 'Email sent successfully'});
-        }
-
         
         return NextResponse.json( data );
     } catch (error) {
-        console.error(error);
-        return NextResponse.json({ error });
-    }
-}
-
-
-export async function GET() {
-    
-    try {
-        const { data } = await resend.emails.send({
-            from: 'onboarding@resend.dev',
-            to: 'kevin.flbt@gmail.com',
-            subject: 'Hello World',
-            html: '<p>Congrats on sending your <strong>first email</strong>!</p>'
-        })
-
-        return NextResponse.json({ data });
-
-    }
-    catch (error) {
         console.error(error);
         return NextResponse.json({ error });
     }
